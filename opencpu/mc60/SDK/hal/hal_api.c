cf578d5779f12692037cc3ae436cb3ebc4e9d7b9
Georgi Angelov <the.wizarda@gmail.com>
2021-12-17 09:08:06 +0200

    Update hal_api.c

--- opencpu/mc60/SDK/hal/hal_api.c
+++ opencpu/mc60/SDK/hal/hal_api.c
@@ -29,52 +29,7 @@ hal_api_t *HAL = NULL;
 /* FIRMWARE DEPENDENT FUNCTIONS */
 static hal_api_t HAL_TABLE[API_COUNT] = {
     {
-        "M66FAR01A12BT",
-        0x100200C0, // FW_VER_ADDR
-
-        0xF026EC59, // soc_getsockopt()
-        0xF026F0F5, // soc_setsockopt()
-
-        /* SSL */
-        0x1007FB35, // TLSv1_0_client_method
-        0x1007FAF5, // TLSv1_1_client_method
-        0x1007FB15, // TLSv1_2_client_method
-        0x1007FA95, // SSLv2_3_client_method
-        0x1007FAB9, // SSLv3_0_client_method
-        0x100C6CCD, // SSL_CTX_new
-        0x100C48C5, // SSL_CTX_free
-        0x100CDDCD, // SSL_CTX_GetError
-        0x100C6F2D, // SSL_CTX_set_verify
-        0x100C6AAD, // SSL_CTX_set_cipher_list
-        0x100C6DC9, // SSL_CTX_use_certificate_chain_file
-        0x100C6D79, // SSL_CTX_use_certificate_buffer
-        0x100C6C81, // SSL_CTX_use_PrivateKey_file
-        0x100C6C2D, // SSL_CTX_use_PrivateKey_buffer
-        0x100C6ECD, // SSL_CTX_load_verify_locations
-        0x100C6E65, // SSL_CTX_load_verify_buffer
-        0x100C6BA5, // SSL_new
-        0x100C500D, // SSL_free
-        0x100C6BF5, // SSL_set_fd
-        0x100D33D5, // SSL_connect
-        0x100CDB69, // SSL_write
-        0x100C65F1, // SSL_read
-        0x100C4D15, // SSL_shutdown
-
-        /* CRYPTO */
-        0x100D8815, // InitMd5
-        0x100CFC45, // Md5Update
-        0x100CF7C7, // Md5Final
-        0x100D87E5, // InitSha
-        0x100CFBED, // ShaUpdate
-        0x100CF721, // ShaFinal
-        0x100D8799, // InitSha256
-        0x100CB4C7, // Sha256Update
-        0x100CBDBD, // Sha256Final
-        0x10154F03, // HmacSetKey
-        0x100B3D1B, // HmacUpdate
-        0x100B3C79, // HmacFinal
-        0x100BFC05, // base64_encode
-        0x100BEFBD, // base64_decode
+        "TODO",
     },
 };
 #pragma GCC diagnostic pop
